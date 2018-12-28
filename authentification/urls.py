8dbb87aad5bcb52857412e0944b2c06b4bfedd2f
horlas <aurelia.gourbere@gmail.com>
2018-12-28 08:55:19 +0100

    setting up the authentication app

--- authentification/urls.py
+++ authentification/urls.py
@@ -5,4 +5,6 @@ app_name = 'authentification'
 
 urlpatterns = [
 
+    path('signin/', views.signin, name='signin'),
+
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signin(request):
+    context = {
+        'days': [1 , 2 , 3],
+    }
+    return render(request, 'authentification/signin.html', context)
