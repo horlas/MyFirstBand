586e9a5205144669799ec35a49ab4802028f42cb
horlas <aurelia.gourbere@gmail.com>
2019-01-22 18:06:38 +0100

    add selenium test of sign in view

--- core/tests/testlive.py
+++ core/tests/testlive.py
@@ -6,6 +6,8 @@ import time
 class MySeleniumTests(StaticLiveServerTestCase):
     fixtures = ['user-data.json']
 
+    TIMEOUT = 2
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -18,19 +20,52 @@ class MySeleniumTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
+    def webdriver_wait(self):
+
+        return WebDriverWait(self.selenium, self.TIMEOUT).until(
+            lambda driver: driver.find_element_by_tag_name('body')
+        )
+
+    def test_signin(self):
+        # we are on the index page , user is not registered
+        self.selenium.get(self.live_server_url)
+        time.sleep(2)
+
+        # user wants to sign-in
+        self.selenium.find_element_by_xpath('//a[@href="/authentication/signup/"]').click()
+
+        # webDriver wait
+        self.webdriver_wait()
+
+        # he accesses to the signin form
+        username_input = self.selenium.find_element_by_id("id_email")
+        username_input.send_keys('michel@gmail.com')
+        time.sleep(2)
+        password_input = self.selenium.find_element_by_id("id_password1")
+        password_input.send_keys('aqwz7418')
+        time.sleep(2)
+        password_input = self.selenium.find_element_by_id("id_password2")
+        password_input.send_keys('aqwz7418')
+        time.sleep(2)
+        # he posts the signin form
+        self.selenium.find_element_by_id("sign_up_button").click()
+        # he is connected
+        self.webdriver_wait()
+        time.sleep(3)
+
     def test_login_logout(self):
 
-        timeout = 2
         # self.selenium.get('%s%s' % (self.live_server_url, '/authentication/accounts/login/'))
         # we are on the index page , user is not connected
         self.selenium.get(self.live_server_url)
         time.sleep(2)
+
         # user wants to connect
         self.selenium.find_element_by_xpath('//a[@href="/authentication/accounts/login/"]').click()
+
         # webDriver wait
-        WebDriverWait(self.selenium, timeout).until(
-            lambda driver: driver.find_element_by_tag_name('body')
-        )
+        self.webdriver_wait()
+
         # he accesses to the login form
         username_input = self.selenium.find_element_by_id("id_username")
         username_input.send_keys('toto@gmail.com')
@@ -42,16 +77,12 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.selenium.find_element_by_xpath('//input[@value="login"]').click()
 
         # he is connected
-        WebDriverWait(self.selenium, timeout).until(
-            lambda driver: driver.find_element_by_tag_name('body')
-        )
+        self.webdriver_wait()
         time.sleep(3)
 
         # he wants to logout
         self.selenium.find_element_by_xpath('//a[@href="/authentication/accounts/logout/"]').click()
-        WebDriverWait(self.selenium, timeout).until(
-            lambda driver: driver.find_element_by_tag_name('body')
-        )
+        self.webdriver_wait()
 
         # he is disconnected so we have the login icon
         self.selenium.find_element_by_xpath('//a[@href="/authentication/accounts/login/"]')
