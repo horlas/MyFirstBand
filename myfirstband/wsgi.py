e5a26ddabd2a0887897e43b69e9de987e8a0a6c9
horlas <aurelia.gourbere@gmail.com>
2019-02-03 13:08:07 +0100

    test with gunicorn

--- myfirstband/wsgi.py
+++ myfirstband/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfirstband1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfirstband.settings')
 
 application = get_wsgi_application()
