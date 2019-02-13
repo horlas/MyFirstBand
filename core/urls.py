b294ff9362514733201021030ba85be5f08e4e11
horlas <aurelia.gourbere@gmail.com>
2019-02-13 14:04:27 +0100

    public profil band is ok + link in index page

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ app_name = 'core'
 urlpatterns = [
 
 path('', views.accueil, name='accueil'),
+path('groupe_public/<slug:slug>', views.BandProfileView.as_view(), name='band_profile_public'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from authentication.models import User
+from band.models import Band, Membership
+from musicians.models import Instrument
+from django.views.generic.detail import DetailView
+
 from musicians.models import UserProfile
 
 # Create your views here.
@@ -9,7 +13,26 @@ def accueil(request):
     context = {}
     last_user = User.objects.all()[:6]
     context['last_users'] = last_user
+    last_band = Band.objects.all()[:6]
+    context['last_bands'] = last_band
 
     return render(request, 'core/index.html', context)
 
+class BandProfileView(DetailView):
 
+    model = Band
+    template_name = 'core/profile_band.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        members = Membership.objects.filter(band=self.object.id)
+
+        data_instru ={}
+        for m in members:
+            instru = Instrument.objects.filter(musician=m.musician).first()
+
+            data_instru[m.musician] = instru
+        context['instru'] = data_instru
+        context['members'] = members
+        return context
+        # Todo : add the instrument to member
