12fb378c0f5e5496796942244a3ed41009216db7
horlas <aurelia.gourbere@gmail.com>
2019-02-17 17:52:12 +0100

    add travis configuration

---
+++ myfirstband/travis_settings.py
@@ -0,0 +1,21 @@
+from myfirstband.settings import *
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': '',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '',
+
+        'HOST': '',
+
+        'PORT': '',
+
+    },
+
+}
